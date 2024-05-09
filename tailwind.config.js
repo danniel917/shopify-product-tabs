688c8c987ce1e7b15571b729c248df3d6522ff9f
gonzacaminos <gonza@gonza.io>
2024-05-09 13:00:15 -0400

    Reduced compilation time by avoinding nested folders on TW

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ import remToPxPlugin from 'tailwindcss-rem-to-px';
 /** @type {import('tailwindcss').Config} */
 export default {
     // Use prefix tw- to avoid conflicts with other Dawn styles
-    // prefix: 'tw-',
+    prefix: 'tw-',
     content: [
-        "./**/*.{js,json,liquid}",
+        "./*/*.{js,json,liquid}", // Warn, only one level deep folders
     ],
     theme: {
         extend: {},
